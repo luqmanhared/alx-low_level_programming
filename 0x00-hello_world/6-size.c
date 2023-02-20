dbe54e11308b83ba6a8af32ed6c99c6795b81817
luqmanhared <haredluqman@gmail.com>
2023-02-20 09:18:17 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,11 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     printf("Size of a char: %lu byte(s)\n", sizeof(char));
