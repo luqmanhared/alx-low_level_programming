0f2dbe57971d39b9f8849a34badb99b0f1ad840b
luqmanhared <haredluqman@gmail.com>
2023-02-23 05:01:41 -0800

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negaive(int i);
+int largest_number(int a, int b, int c);
+int convert day(int month, int day);
+void print_remainig_days(int month, int day, int year);
+
+#endif /* MAIN_H */
