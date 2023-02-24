f902f63e872a66334c09cac380b0426469c7c015
luqmanhared <haredluqman@gmail.com>
2023-02-24 12:48:06 -0800

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character
+ * _isupper - check for uppercase.
+ * @c: argument
  * Return: Always 0.
  */
 int _isupper(int c)
