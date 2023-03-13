d260c4fd29b314a4b0a22a72061a3386746e464a
luqmanhared <haredluqman@gmail.com>
2023-03-13 02:43:04 -0700

    libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,19 +1,14 @@
 #include "main.h"
-
 /**
- * _isupper - detects if c is uppercase
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
-
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
 }
