7e4c54f435d0703e0797e86823e51862e29bee98
luqmanhared <haredluqman@gmail.com>
2023-04-10 14:04:36 -0700

    Implement function to create and write content to a file according to specified requirements

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,40 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes content to it.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to the text to write to the file.
+ * create_file - Creates a file with the given filename and text content.
  *
- * Return: If the function fails -1.
- *         Otherwise - 1.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, len = 0, w;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[len])
+		while (text_content[len] != '\0')
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	w = write(fd, text_content, len);
-	if (w == -1)
+	if (len > 0)
 	{
-		close(fd);
-		return (-1);
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
-
 	return (1);
 }
