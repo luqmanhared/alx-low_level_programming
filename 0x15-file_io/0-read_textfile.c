a03edf467cc41e62a3a0347950d6cdab7cfcbb52
luqmanhared <haredluqman@gmail.com>
2023-04-10 11:46:02 -0700

    Implement function to read and print text file contents

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,29 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * create_file - function that creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * read_textfile - Reads a text file and writes its content to the standard output.
+ * @filename: Name of the text file to read.
+ * @num_letters: Maximum number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or
+ *         0 if the function fails or the filename is NULL.
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t num_letters)
 {
-	int file_descriptor, result;
-	size_t length = 0;
+	char *buffer;
+	ssize_t file_desc, read_letters, printed_letters;
 
-	if (filename == NULL)
-		return (-1);
+	file_desc = open(filename, O_RDONLY);
+	if (file_desc == -1)
+		return (0);
 
-	file_descriptor = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
-	if (file_descriptor == -1)
-		return (-1);
+	buffer = malloc(sizeof(char) * num_letters);
+	read_letters = read(file_desc, buffer, num_letters);
+	printed_letters = write(STDOUT_FILENO, buffer, read_letters);
 
-	if (text_content != NULL)
-	{
-		while (text_content[length] != '\0')
-			length++;
+	free(buffer);
+	close(file_desc);
 
-		result = write(file_descriptor, text_content, length);
-		if (result == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
-	}
-
-	close(file_descriptor);
-	return (1);
+	return (printed_letters);
 }
