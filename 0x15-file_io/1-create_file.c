6011ecb13b7be64d4ef758a7ce251339338f2470
luqmanhared <haredluqman@gmail.com>
2023-04-10 17:14:41 -0700

    Implement function to create and write content to a file according to

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file
+ * create_file - Creates a file with specified permissions and writes the
+ *               provided content to the file. If the file exists, it is
+ *               truncated.
  * @filename: A pointer to the name of the file to create
- * @text_content: A pointer to a string to write to the file
+ * @text_content: A pointer to a NULL-terminated string to write to the file
  *
- * Return: -1 if the function fails, 1 if successful
+ * Return: 1 on success, -1 on failure (file cannot be created, file cannot
+ *         be written, write fails, etc.)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, write_res, length = 0;
+	int file_descriptor, write_status, length = 0;
 
 	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		while (text_content[length])
 			length++;
+
+		write_status = write(file_descriptor, text_content, length);
+
+		if (write_status == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write_res = write(file, text_content, length);
-
-	if (file == -1 || write_res == -1)
-		return (-1);
-
-	close(file);
-
+	close(file_descriptor);
 	return (1);
 }
