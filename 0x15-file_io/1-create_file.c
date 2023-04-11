d717702c686fb2fb071c6efa278d00dfdeb9a785
luqmanhared <haredluqman@gmail.com>
2023-04-10 17:04:56 -0700

    Implement function to create and write content to a file according to

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,35 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * create_file_alt - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - Generates a new file and writes content to it
+ * @filename: A pointer to the string representing name of the file to create
+ * @text_content: A pointer to the string that will be written to the new file
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: Returns -1 if an error occurs, 1 if the it executes successfully
  */
-int create_file_alt(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	FILE *file_ptr;
-	size_t len = 0;
+	int file_descriptor, write_status, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		while (text_content[length])
+			length++;
 	}
 
-	file_ptr = fopen(filename, "w");
-	if (file_ptr == NULL)
-		return (-1);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_status = write(file_descriptor, text_content, length);
 
-	if (text_content != NULL)
+	if (file_descriptor == -1 || write_status == -1)
 	{
-		if (fwrite(text_content, sizeof(char), len, file_ptr) != len)
-		{
-			fclose(file_ptr);
-			return (-1);
-		}
+		close(file_descriptor);
+		return (-1);
 	}
 
-	fclose(file_ptr);
+	close(file_descriptor);
 
 	return (1);
 }
