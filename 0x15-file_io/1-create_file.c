d14ccc674a01eefe64098ec9bf901c5406f7f00a
luqmanhared <haredluqman@gmail.com>
2023-04-14 11:51:59 -0700

    Implemented function to create and write to files with proper permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,27 +13,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_dp, res;
+	int file_desc, status;
+	size_t content_length;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_dp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (file_dp == -1)
+	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		res = write(file_dp, text_content, strlen(text_content));
+		content_length = strlen(text_content);
+		status = write(file_desc, text_content, content_length);
 
-		if (res == -1)
+		if (status == -1)
 		{
-			close(file_dp);
+			close(file_desc);
 			return (-1);
 		}
 	}
 
-	close(file_dp);
+	close(file_desc);
 	return (1);
 }
