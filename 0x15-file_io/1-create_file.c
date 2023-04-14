66832681e953282494d82af01e7081079aaf5244
luqmanhared <haredluqman@gmail.com>
2023-04-14 11:07:04 -0700

    Implemented function to create and write to files with proper permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,41 @@
-#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * create_file - Creates a file and writes text inside it.
- *
- * @filename: Has the name of the file that will be created.
- * @text_content: Null-terminated string to write to file
+ * create_file - creates a file and writes the specified content
+ * @filename: name of the file to create
+ * @text_content: null-terminated string to write into the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length = 0, written_bytes;
+	int file_desc, status;
+	size_t content_length;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (file_desc == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length]; length++)
-			;
+		content_length = strlen(text_content);
+		status = write(file_desc, text_content, content_length);
 
-		written_bytes = write(fd, text_content, length);
-		if (written_bytes == -1)
+		if (status == -1)
 		{
-			close(fd);
+			close(file_desc);
 			return (-1);
 		}
 	}
 
-	close(fd);
+	close(file_desc);
 	return (1);
 }
